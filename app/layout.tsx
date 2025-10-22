0cb314bb2b5a22aa192be365a3d7abb8623617c3
Jose Gale <josedavidgale@gmail.com>
2025-10-22 12:45:51 +0100

    Setup tRPC

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TRPCProvider } from "@/trpc/client";
+import { HydrateClient } from "@/trpc/server";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +26,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body style={{ isolation: "isolate" }}>
+        <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <TRPCProvider>
+            <HydrateClient>{children}</HydrateClient>
+          </TRPCProvider>
+        </div>
       </body>
     </html>
   );
