8ec579913eb468b5d48ff32844b10ea0c93cd2b8
Jose Gale <josedavidgale@gmail.com>
2025-10-24 10:52:58 +0100

    Fix Drizzle Adapter NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,18 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { db } from "@/app/schema";
+import { accounts, db, sessions, users, verificationTokens } from "@/app/schema";
 // You can add more providers (Google, Credentials, etc.)
 
 const handler = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db
+    , {
+      usersTable: users,
+      accountsTable: accounts,
+      sessionsTable: sessions,
+      verificationTokensTable: verificationTokens,
+    }
+  ),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
@@ -14,9 +21,9 @@ const handler = NextAuth({
   ],
   // Optional config
   secret: process.env.NEXTAUTH_SECRET,
-//   pages: {
-//     signIn: "/auth/signin", // optional custom sign-in page
-//   },
+  //   pages: {
+  //     signIn: "/auth/signin", // optional custom sign-in page
+  //   },
 });
 
 export { handler as GET, handler as POST };
